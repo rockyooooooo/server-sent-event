89ee6a33529c9c8df14a9d8636b67d4cdfc601f9
allen.liao <allen.liao@genie-networks.com>
2023-10-11 14:12:54 +0800

    try using fetch

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,22 @@ const server = http.createServer((req, res) => {
     "Content-Type": "text/event-stream",
     "Access-Control-Allow-Origin": "*"
   })
-  res.end('data: Hello world!\n\n')
+
+  // setInterval(() => {
+  //   res.write(`data: ${new Date()}\n\n`)
+  // }, 1000)
+
+  res.write('retry: 1000\n\n')
+
+  res.write('event: hello\n')
+  res.write('data: Hello world!\n\n')
+
+  res.write('event: json\n')
+  res.write('data: {\n')
+  res.write('data: "msg": "YOYOYO!"\n')
+  res.write('data: }\n\n')
+
+  res.write('event: bye\n')
+  res.end('data: Bye world!\n\n')
 })
 server.listen(3000)
--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,37 @@
-const sse = new EventSource('http://localhost:3000/')
+const url = 'http://localhost:3000/'
 
-sse.addEventListener("message", (e) => {
-  console.log(e.data)
-})
+// const sse = new EventSource(url)
+
+// sse.addEventListener("message", (e) => {
+//   console.log(e.data)
+// })
+// sse.addEventListener("hello", (e) => {
+//   console.log(e.data)
+// })
+// sse.addEventListener("json", (e) => {
+//   const json = JSON.parse(e.data)
+//   console.log(json)
+// })
+// sse.addEventListener("bye", (e) => {
+//   console.log(e.data)
+// })
+
+fetch(url)
+  .then(response => {
+    const reader = response.body
+      .pipeThrough(new TextDecoderStream())
+      .getReader();
+
+    const stream = new ReadableStream({
+      start() {
+        reader.read().then((rlt) => {
+          console.log('start: ', rlt)
+        });
+      },
+      pull() {
+        reader.read().then((rlt) => {
+          console.log('pull: ', rlt)
+        })
+      }
+    })
+  });
